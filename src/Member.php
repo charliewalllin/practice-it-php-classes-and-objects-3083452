8640322f474ac615a8797953b995a6a31af7fb75
Charlie Wallin <charliekwallin@gmail.com>
2023-07-17 17:04:08 -0400

    Update Member.php

--- src/Member.php
+++ src/Member.php
@@ -1,1 +1,21 @@
 <?php
+
+public class Member {
+
+  public username;
+  // admin or member
+  public role;
+  
+  private function addWorkspaceMember() {
+  }
+
+  private function createWorkspace() {
+  }
+
+  private function createChat() {
+  }
+
+  private function postMessageToChat() {
+  }
+  
+}
